024d81e62573f7da573b20b7cebc3a8c71d05356
Malik <mertus.malik@gmail.com>
2022-03-19 23:40:49 -0400

    Added Readme

--- home-page/script.js
+++ home-page/script.js
@@ -1,29 +1,44 @@
-//var pictures = document.getElementsByClassName('.img');
+//assigning js variables to html elements
 var myPig = document.getElementById('about-text');
-var myA = document.getElementById('resume');
-var myB = document.getElementById('github');
-var myC = document.getElementById('linkedin');
-var myD = document.getElementById('personal-projects');   
+var myResume = document.getElementById('resume');
+var myGithub = document.getElementById('github');
+var myLinkedin = document.getElementById('linkedin');
+var myPersonal = document.getElementById('personal-projects');   
 
 /* for(var i=0;i < pictures.length;i++){
     pictures[i].addEventListener('mouseover',chicken);
 } */
 
-//myPig.addEventListener('mouseover',chicken);
-myA.addEventListener('mouseover',chicken);
-myB.addEventListener('mouseover',chicken);
-myC.addEventListener('mouseover',chicken);
-myD.addEventListener('mouseover',chicken);
-myA.addEventListener('mouseout',steak);
-myB.addEventListener('mouseout',steak);
-myC.addEventListener('mouseout',steak);
-myD.addEventListener('mouseout',steak); 
-// pictures.addEventListener('mouseover',chicken());
+// event listeners for each img, on img
+myResume.addEventListener('mouseover',resume);
+myGithub.addEventListener('mouseover',github);
+myLinkedin.addEventListener('mouseover',linkedin);
+myPersonal.addEventListener('mouseover',personalProjects);
 
-function chicken() {
-myPig.textContent = "youre nosy arent you";
+// event listeners for mouse moving off img
+myResume.addEventListener('mouseout',steak);
+myGithub.addEventListener('mouseout',steak);
+myLinkedin.addEventListener('mouseout',steak);
+myPersonal.addEventListener('mouseout',steak); 
+
+// functions that change about text according to image hovered over
+function resume() {
+myPig.textContent = "Take a look at my resume";
 };
 
+function github() {
+    myPig.textContent = "1 Commit a day is the best way";
+}
+
+function linkedin() {
+    myPig.textContent = "See me in a dress shirt";
+}
+
+function personalProjects() {
+    myPig.textContent = "Coding projects and things";
+}
+
+// function to reset the about box text after hover
 function steak() {
     myPig.textContent = "Software engineer extraordinaire";
 }
