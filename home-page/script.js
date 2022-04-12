0487cde5c03f333e49488b1093b59fd6b1a8ea90
Malik <mertus.malik@gmail.com>
2022-04-12 10:03:38 -0400

    updated var names

--- home-page/script.js
+++ home-page/script.js
@@ -1,11 +1,11 @@
 //assigning js variables to html elements
-var myPig = document.getElementById('about-text');
+var AboutText = document.getElementById('about-text');
 var myResume = document.getElementById('resume');
 var myGithub = document.getElementById('github');
 var myLinkedin = document.getElementById('linkedin');
 var myContact = document.getElementById('contact');   
 
-myPig.textContent = "Hover over an icon to learn more!";
+AboutText.textContent = "Hover over an icon to learn more!";
 
 /* for(var i=0;i < pictures.length;i++){
     pictures[i].addEventListener('mouseover',chicken);
@@ -18,38 +18,38 @@ myLinkedin.addEventListener('mouseover',linkedin);
 myContact.addEventListener('mouseover',contactPage);
 
 // event listeners for mouse moving off img
-myResume.addEventListener('mouseout',steak);
-myGithub.addEventListener('mouseout',steak);
-myLinkedin.addEventListener('mouseout',steak);
-myContact.addEventListener('mouseout',steak); 
+myResume.addEventListener('mouseout',ResetText);
+myGithub.addEventListener('mouseout',ResetText);
+myLinkedin.addEventListener('mouseout',ResetText);
+myContact.addEventListener('mouseout',ResetText); 
 
 // functions that change about text according to image hovered over
 function resume() {
-myPig.textContent = "Take a look at my resume";
-myPig.style.opacity = 1;
+AboutText.textContent = "Take a look at my resume";
+AboutText.style.opacity = 1;
 };
 
 function github() {
-    myPig.textContent = "1 Commit a day";
-    myPig.style.opacity = 1;
+    AboutText.textContent = "1 Commit a day";
+    AboutText.style.opacity = 1;
 }
 
 function linkedin() {
-    myPig.textContent = "See me in a dress shirt";
-    myPig.style.opacity = 1;
+    AboutText.textContent = "See me in a dress shirt";
+    AboutText.style.opacity = 1;
 }
 
 function contactPage() {
-    myPig.textContent = "Reach out to me!";
-    myPig.style.opacity = 1;
+    AboutText.textContent = "Reach out to me!";
+    AboutText.style.opacity = 1;
 }
 
 // function to reset the about box text after hover
-function steak() {
-    myPig.textContent = "Hover over an icon to learn more!"
-    myPig.style.opacity = 1;
-    myPig.style.transition = "ease-in";
-    myPig.style.transitionDuration = 2;
+function ResetText() {
+    AboutText.textContent = "Hover over an icon to learn more!"
+    AboutText.style.opacity = 1;
+    AboutText.style.transition = "ease-in";
+    AboutText.style.transitionDuration = 2;
 }
 
 // function to fill blank project slots
