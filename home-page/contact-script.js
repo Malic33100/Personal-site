151f037d81b30fac33e56f1bf0ea8503a50efca8
Malik <mertus.malik@gmail.com>
2022-04-27 18:18:29 -0400

    added nodemailer

--- home-page/contact-script.js
+++ home-page/contact-script.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("form");
 
+
 var firstName = form.elements["first"].value;
 var lastName = form.elements["second"].value;
 var email = form.elements["email"].value;
@@ -7,11 +8,29 @@ var telly = form.elements["telly"].value;
 var file = form.elements["file"].value;
 var message = form.elements["message-box"].value;
 
-var formElement = document.querySelector("form");
-var request = new XMLHttpRequest();
-request.open("POST", "submitform.php");
-//request.send(new FormData(firstName));
 
+const nodemailer = require('nodemailer');
+const mailgun = require('nodemailer-mailgun-transport');
 
+const transporter = nodemailer.createTransport(mailgun({
+    auth: {
+        api_key: process.env.API_KEY,
+        domain: process.env.DOMAIN,
+    }
+}));
 
-form.addEventListener("submit", )
+const mailOptions = {
+    from: email, //user's mail address, you can make it dynamic from contact form
+    to: "mertus.malik@gmail.com", //the mail address you authorized in mailgun (receipient mail address)
+    subject: "", //maybe from contact for
+    text: form, //maybe from contact for
+};
+
+transporter.sendMail(mailOptions, (error, response) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Message Sent!");
+        alert("Message sent succesfully.")
+    }
+});
